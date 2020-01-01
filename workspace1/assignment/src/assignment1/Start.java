e5c6bbc986b6d8c3d53f083cac59bde54d19ad9a
Wang961506 <56723331+Wang961506@users.noreply.github.com>
2020-01-01 22:40:21 +0000

    added the function of Time Bomb,but the function of judging the winner has some problem

--- workspace1/assignment/src/assignment1/Start.java
+++ workspace1/assignment/src/assignment1/Start.java
@@ -6,11 +6,11 @@ public static boolean flag1=true;
  public void display(){
 	 Scanner stdin=new Scanner(System.in);
 	 System.out.println("************************************");
-	 System.out.println("     welcome to play this game");
-	 System.out.println("     1.start game");
-	 System.out.println("     2.quit ");
+	 System.out.println("     Welcome To Play This Game");
+	 System.out.println("     1.Start Game");
+	 System.out.println("     2.Quit ");
 	 System.out.println("************************************");
-	 System.out.print("please enter your choice:");
+	 System.out.print("Please Enter Your Choice:");
 	 int myChoice=stdin.nextInt();
 	 System.out.println();
 	 switch(myChoice)
@@ -18,21 +18,30 @@ public static boolean flag1=true;
 	 case 1: System.out.println("************Start Game***************");
 	         System.out.println();
 	         break;
-	 case 2: System.out.println("end this game");
+	 case 2: {
+		     System.out.println("************End   Game***************");
 	         flag1=false;
 	         break;
-	 default:System.out.println("the instruction is wrong");
+	 }
+	 default:System.out.println("The Instruction Is Wrong");
 	 } 
 	 }
 public static void main(String[] args){
 	Start start=new Start();
 	boardDisplay boarddisplay=new boardDisplay();
 	process process1=new process();
-    while(true)
+	while(flag1)
 	{
-
-		start.display();
+	start.display();
+    while(flag1)
+	{
 		process1.startGame();
+		if(process1.gameover)
+		{
+			process1.gameover=false;
+		}
+		break;
+	}
 	}
 }
 }
@@ -42,6 +51,7 @@ class player{
     String shape;
     int column;
     char blitz;
+    char timeBomb;
     public player(String name,String shape){
     	this.name=name;
     	this.shape=shape;
@@ -52,9 +62,11 @@ public void  getLocation(){    //落子
 	while(flag2)
 	{
 		if(blitz=='B')
-			System.out.print("Blitz please select column >");
+			System.out.print("Blitz Please Select Column >");
+		else if(timeBomb=='T')
+			System.out.print("Time Bomb Please Select Column >");
 		else
-        System.out.print("please player "+name+" enter the column coordinates:");
+        System.out.print("Player "+name+" Select Column>");
     //please player "+name+" enter the column coordinates:
 //	column=stdin.nextInt();
 	String inputString=stdin.nextLine();
@@ -73,11 +85,20 @@ public void  getLocation(){    //落子
 			column=inputchar-'0';
 			flag2=false;
 		}
-		else
+		else 
 		{
+			if(inputchar=='B')
+			{
 		   blitz=inputchar;
 		   flag2=false;
+			}
+			else
+			{
+				timeBomb=inputchar;
+				flag2=false;
+			}
 		}
+
 	}
 }
 }
@@ -87,6 +108,9 @@ public void  getLocation(){    //落子
     
 class boardDisplay{
 public String[][] board=new String[10][9];
+int columnofbomb=0;
+int rowofbomb=0;
+//process process1=new process();
 public void printchessboard(){
 	for(int i=0;i<10;i++)
 	{
@@ -118,18 +142,24 @@ class process{
 	player player1=new player("1","X");
 	player player2=new player("2","O");
 	player turn=null;
-	boolean gameover=false;
+	static boolean gameover=false;
+	int time=0;
+	int time1=0;
 	int[] count={1,1,1,1,1,1,1,1,1};
 	boardDisplay boarddisplay=new boardDisplay();
+	Start start1=new Start();
 	public void whobegin()
 	{
 		int name;
 		Scanner stdin=new Scanner(System.in);
 		boolean Isselect=true;
 		while(Isselect){
-		System.out.println("please choose the number of player who starts playing first:1.player1 OR 2.player2");
-		System.out.print("enter the number:");
+		System.out.println();
+		System.out.println("*********************************************************************************");
+		System.out.println("Please Choose Who Starts Playing First:1.Player 1 OR 2.Player 2");
+		System.out.print("Enter the number:");
 		name=stdin.nextInt();
+		System.out.println("*********************************************************************************");
 		switch(name){
 		case 1:turn=player1;
 		       return;
@@ -141,7 +171,31 @@ class process{
 	}
 	
     public void putchesspieces(player players,boardDisplay boarddisplay){
-    	players.getLocation();//得到需要下的子的列数
+    	players.getLocation();//得到需要下的子的列数,或者炸弹和消列的命令
+    	if(players.timeBomb=='T')
+    	{
+    		if(players.column!=0)
+        	{
+        	if(count[players.column-1]>9){
+        		System.out.println("the column is full,please retype it");
+        	}
+        	else if(boarddisplay.board[9-count[(players.column-1)]+1][players.column-1]=="_"){
+        			boarddisplay.board[9-count[(players.column-1)]+1][players.column-1]="*";
+        	      	boarddisplay.columnofbomb=players.column-1;//列
+        	      	boarddisplay.rowofbomb=9-count[(players.column-1)]+1;//行 get the row of bomb
+        			count[players.column-1]++;
+        	      	time1=time;
+        			time++;
+        			players.column=0;
+        			players.timeBomb=0;
+        			if(turn==player1)
+        				turn=player2;
+        			else
+        				turn=player1;
+        	}	
+        	}
+    	}
+    	else{
     	if(players.column!=0)
     	{
     	if(count[players.column-1]>9){
@@ -150,12 +204,14 @@ class process{
     	else if(boarddisplay.board[9-count[(players.column-1)]+1][players.column-1]=="_"){
     			boarddisplay.board[9-count[(players.column-1)]+1][players.column-1]=players.shape;
     			count[players.column-1]++;
+    			time++;
     			players.column=0;
     			if(turn==player1)
     				turn=player2;
     			else
     				turn=player1;
     	}	
+    	}
     	}
     	if(players.blitz=='B')
     	{
@@ -164,7 +220,12 @@ class process{
     		for(int i=1;i<10;i++)
     		 boarddisplay.board[i][players.column-1]="_";
     		 count[players.column-1]=1;
+    		 time++;
     		players.blitz=0;
+			if(turn==player1)
+				turn=player2;
+			else
+				turn=player1;
     	}
     }
     
@@ -172,15 +233,26 @@ class process{
     	System.out.println("Begin The Game");
     	boarddisplay.printchessboard();//打印棋盘
     	whobegin();//选择谁先开始
-    	while(true)
+    	while(start1.flag1)
     	{ 
     		
-    		putchesspieces(turn,boarddisplay);//下子
+    		putchesspieces(turn,boarddisplay);//下棋子
+    		TimeBomb(boarddisplay);
+    		if(turn.timeBomb!='T')
     		boarddisplay.printchessboard();
     		judge(boarddisplay);
     		if(gameover)
     		{
+    			for(int i=0;i<boarddisplay.board.length;i++)
+    				for(int j=0;j<boarddisplay.board[i].length;j++)
+    				{
+    					if(boarddisplay.board[i][j]!="_")
+    						boarddisplay.board[i][j]="_";
+    					count[j]=1;
+    				}
+    			System.out.println("********************************************************");
     			System.out.println("Game Over！Player "+turn.name+" win");
+    			System.out.println("********************************************************");
     			break;
     		}
     	}
@@ -221,11 +293,46 @@ class process{
     		}
     	}
     }
-}
-
-
-class BlitzandBomb{
-	
+    
+    public void TimeBomb(boardDisplay boarddisplay){//rowofbomb=players.column-1,columnofbomb=9-count[(players.column-1)]+1;
+        if((time-time1)==5 && time1!=0)
+        {
+        	time1=0;
+        	for(int i=boarddisplay.rowofbomb-1;i<boarddisplay.rowofbomb+2;i++)//对炸弹区域清空
+        	{
+        		for(int j=boarddisplay.columnofbomb-1;j<boarddisplay.columnofbomb+2;j++)
+        		{
+        			if(i>0&&i<10)
+        				if(j>=0&&j<9)
+        				{
+        					if(boarddisplay.board[i][j]!="_")
+        					{
+        					boarddisplay.board[i][j]="_";
+        					count[j]--;
+        					}
+        				}
+        		}
+        	}
+     	
+        	for(int j=boarddisplay.columnofbomb-1;j<boarddisplay.columnofbomb+2;j++)
+        	{
+        		for(int i=boarddisplay.rowofbomb-1;i<boarddisplay.rowofbomb+2;i++)
+        		{
+        			if(j>=0 && j<9 &&i>=0 && i<10)
+        			{
+        				if((i-3)>=0)
+        				{
+        					 if(boarddisplay.board[i-3][j]!="_")
+        					 {
+              				     boarddisplay.board[i][j]=boarddisplay.board[i-3][j];
+              				     boarddisplay.board[i-3][j]="_";
+        					 }
+        				}
+        			}
+        		}
+        	}
+        } 
+    }
 }
 
 
